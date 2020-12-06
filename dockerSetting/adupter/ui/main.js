95e0525b7d96bce0f24ac62c379d201ddd86a08f
b2blosangeles <b2blosangeles@gmail.com>
2020-12-05 16:12:40 -0800

    Create main.js

---
+++ dockerSetting/adupter/ui/main.js
@@ -0,0 +1,27 @@
+<template>
+    <div>  
+      adupter main
+    </div>
+</template>
+ 
+<script>
+module.exports = {
+    data: function() {
+        return {
+        }
+    },
+    mounted () {},
+    methods :{
+    },
+    components: VUEApp.loadComponents({
+        LOAD    : {
+        }, 
+        TPL :{
+        }
+    })
+}
+</script>
+ 
+<style>
+
+</style>
