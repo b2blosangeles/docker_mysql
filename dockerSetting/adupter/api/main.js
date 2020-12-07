b40aa737cdcba58a97a379661c3574f4d192867a
b2blosangeles <b2blosangeles@gmail.com>
2020-12-06 20:48:05 -0800

    Update main.js

--- dockerSetting/adupter/api/main.js
+++ dockerSetting/adupter/api/main.js
@@ -1,8 +1,15 @@
 (function() {
-    var obj = function(path, pkg, cfg) {
-        
+    var obj = function(myPath, pkg, cfg) {
+        var me = this,
+            CP = new pkg.crowdProcess(),
+            MYSQL = pkg.require(myPath + '/vendor/mysql/node_modules/mysql');
         this.run = (callback) => {
-            callback('cfg');
+            var connection = MYSQL.createConnection(cfg);
+            var sql_str = 'SELECT * from user';
+            connection.query(sql_str, function (error, results, fields) {
+                connection.end();
+                callback((error) ? error : results);
+            });
         }
     }
     module.exports = obj;
