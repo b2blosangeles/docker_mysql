dbc8cef9107181f6b2e5c94100c9dfb753c309e0
b2blosangeles <b2blosangeles@gmail.com>
2020-12-07 13:02:23 -0800

    Update main.vue

--- dockerSetting/adupter/ui/main.vue
+++ dockerSetting/adupter/ui/main.vue
@@ -43,7 +43,8 @@ module.exports = {
         _post() {
         },
         isAppUserReady () {
-            return (this.appUserList.length) ?  true : false;
+            return false;
+           // return (this.appUserList.length) ?  true : false;
         },
         getAllDatabase(url) {
             let me = this;
