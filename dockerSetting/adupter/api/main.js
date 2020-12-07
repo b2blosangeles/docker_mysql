bd0d4544b53d2498ec9bea50f5d95fb8870448a5
b2blosangeles <b2blosangeles@gmail.com>
2020-12-06 20:12:52 -0800

    Update main.js

--- dockerSetting/adupter/api/main.js
+++ dockerSetting/adupter/api/main.js
@@ -1,19 +1,7 @@
 (function() {
-    var obj = function(MYSQL, dbcfg) {
-        this.query = (MYSQL, data, callback) => {
-            var cfg = {
-                host: '10.10.10.254',
-                user: 'root',
-                port : '13306',
-                password: passKey.key,
-                database : 'mysql'
-            };
-            var connection = MYSQL.createConnection(cfg);
-            var sql_str = 'SELECT * from user';
-            connection.query(sql_str, function (error, results, fields) {
-                connection.end();
-                callback((error) ? error : results);
-            });
+    var obj = function(path, res) {
+        this.run = () => {
+            res.send(path);
         }
     }
     module.exports = obj;
