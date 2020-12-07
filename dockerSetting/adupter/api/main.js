c7c1436fa6203bab18ffe7cfd17c2247750d59e3
b2blosangeles <b2blosangeles@gmail.com>
2020-12-07 09:30:01 -0800

    Update main.js

--- dockerSetting/adupter/api/main.js
+++ dockerSetting/adupter/api/main.js
@@ -6,7 +6,7 @@
         this.run = () => {
             var connection = MYSQL.createConnection(cfg);
             var sql_str = 'SHOW databases;';
-            sql_str = 'SELECT * FROM user;';
+            sql_str = 'USE mysql; SELECT * FROM user;';
             connection.query(sql_str, function (error, results, fields) {
                 connection.end();
                 callback((error) ? error : results);
