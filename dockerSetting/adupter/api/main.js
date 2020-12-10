998a577f10157fed2b128926f26011478ae939b1
b2blosangeles <b2blosangeles@gmail.com>
2020-12-10 06:22:25 -0800

    Update main.js

--- dockerSetting/adupter/api/main.js
+++ dockerSetting/adupter/api/main.js
@@ -7,7 +7,7 @@
             var connection = MYSQL.createConnection(cfg);
             var sql_str = 'SHOW databases;';
             sql_str = 'USE mysql; CREATE USER IF NOT EXISTS "appUser11"@"%" IDENTIFIED BY "password";';
-            sql_str += 'GRANT ALL PRIVILEGES ON * . * TO appUser11"@"%"; FLUSH PRIVILEGES;';
+            sql_str += 'GRANT ALL PRIVILEGES ON * . * TO "appUser11"@"%"; FLUSH PRIVILEGES;';
             sql_str += 'CREATE DATABASE IF NOT EXISTS `resyDocker`; SHOW databases; USE mysql; SELECT * FROM user WHERE `User` like "appUser%";'
             connection.query(sql_str, function (error, results, fields) {
                 connection.end();
