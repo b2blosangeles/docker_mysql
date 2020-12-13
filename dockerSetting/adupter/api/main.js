73289173bade49c710df48684378326fd02d0289
b2blosangeles <b2blosangeles@gmail.com>
2020-12-13 15:36:23 -0800

    Update main.js

--- dockerSetting/adupter/api/main.js
+++ dockerSetting/adupter/api/main.js
@@ -6,7 +6,11 @@
             MYSQL = require(__dirname + '/vendor/mysql/node_modules/mysql');
         
         this.call = (opt, dockerEnv) => {
-            me[opt](dockerEnv);
+            try {
+                me[opt](dockerEnv);
+            } catch(e) {
+                callback(__dirname + ':' + e.message);
+            }  
         }      
         this.run = (dockerEnv) => {
             try {
