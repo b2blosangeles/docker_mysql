d845d50ba3fa0745ee2f164a29ec106b8864faaf
b2blosangeles <b2blosangeles@gmail.com>
2020-12-13 16:13:09 -0800

    Update main.vue

--- dockerSetting/adupter/ui/main.vue
+++ dockerSetting/adupter/ui/main.vue
@@ -11,7 +11,7 @@
  
 <script>
 module.exports = {
-    props : ['item', 'pluginPath'],
+    props : ['item', 'plugin-path'],
     data: function() {
         return {
             root :  this.$parent.root,
