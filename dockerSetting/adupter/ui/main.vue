ced5f6e611e4fcd63516cdc51db13012f9c9ab44
b2blosangeles <b2blosangeles@gmail.com>
2020-12-13 16:22:53 -0800

    Update main.vue

--- dockerSetting/adupter/ui/main.vue
+++ dockerSetting/adupter/ui/main.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="adupterBody border border-info br-3 m-1 p-3 rounded-lg alert-info">  
       adupter body <button class="btn btn-warning" v-if="!isAppUserReady()">Add User</button>
-      {{pluginpath}}
+      {{pluginPath}}
       <hr/> 
       {{item}}
       <hr/>
